b6f5a4021baac2c606a013a003cd030db7f10c68
mmikelic <mikelicmichael@gmail.com>
2022-06-02 21:24:26 -0400

    create models

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const Blog = require('./Blog');
+
+const Comment = require('./Comment');
+
+const User = require('./User');
+
+
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment,{
+  foreignKey: 'blog_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+module.exports = { User, Blog, Comment };
